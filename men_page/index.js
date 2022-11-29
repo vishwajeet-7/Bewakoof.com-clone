c3413a05847fe9129c06a8aef8d4cc79a84e3b41
MOHDSHABANKHAN <shabhankhan123456@gmail.com>
2022-11-29 22:26:08 +0530

    first commit

---
+++ men_page/index.js
@@ -0,0 +1,8 @@
+var mensData = [
+    {
+        image_url:
+          "https://images.bewakoof.com/uploads/grid/app/category-box-new-printed-tees-1668772678.jpg",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+
+    },
+];
